9b402a941cb601801243e3fa9d7f141f3b2300c9
Irene <grim6ace@gmail.com>
2025-07-30 00:50:29 +0300

    Ver 6

--- dribbble-clone.js
+++ dribbble-clone.js
@@ -1,9 +1,12 @@
-/* SEARCH BOX PLACEHOLDER */
+/* SEARCH BOX PLACEHOLDER AND TRENDS' LINKS */
 
 const searchInput = document.querySelector('.search-box-input');
 const shots = document.getElementById('shots');
 const designers = document.getElementById('designers');
 const services = document.getElementById('services');
+const shotsTrends = document.querySelector('.shots-trends');
+const designersTrends = document.querySelector('.designers-trends');
+const servicesTrends = document.querySelector('.services-trends');
 
 function changePlaceholder() {
   if (shots.checked) {
@@ -13,10 +16,22 @@ function changePlaceholder() {
   } else if (services.checked) {
     searchInput.placeholder = 'What do you need designed?';
   }
+
+   shotsTrends.style.display = 'none';
+   designersTrends.style.display = 'none';
+   servicesTrends.style.display = 'none';
+
+   if (shots.checked) {
+    shotsTrends.style.display = 'block';
+  } else if (designers.checked) {
+    designersTrends.style.display = 'block';
+  } else if (services.checked) {
+    servicesTrends.style.display = 'block';
+  }
 };
 
 shots.addEventListener('change', changePlaceholder);
 designers.addEventListener('change', changePlaceholder);
 services.addEventListener('change', changePlaceholder);
 
-
+changePlaceholder();
